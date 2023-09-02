4b1fcaf004a71ecf2e670f78eddd76e18eae5c4f
Tushar Saini <s2sharpit@gmail.com>
2023-09-02 23:27:41 +0530

    Time: 3 ms (99.75%), Space: 47.1 MB (85.91%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.java
@@ -1,13 +1,17 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int res = 0;
-        
-        for(int i = 0 ;i<items.size();i++){
-            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
-            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
-            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
+        int index = 0;
+
+        // if (ruleKey.equals("type")) index = 0;
+        if (ruleKey.equals("color")) index = 1;
+        if (ruleKey.equals("name")) index = 2;
+
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).get(index).equals(ruleValue))
+                res++;
         }
-        
+
         return res;
     }
 }

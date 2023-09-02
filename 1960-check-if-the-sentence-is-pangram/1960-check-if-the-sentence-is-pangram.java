bef3759064462a650abf6ef806928025db2eb4f4
Tushar Saini <s2sharpit@gmail.com>
2023-09-02 21:35:44 +0530

    Time: 0 ms (100%), Space: 41 MB (14.78%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         if (sentence.length() < 26) {
 			return false;
-		}
-		String alphas = "abcdefghijklmnopqrstuvwxyz";
-		for (int i = 0; i < alphas.length(); i++) {
-			if (sentence.indexOf(alphas.charAt(i)) == -1) {
+        }
+		for (char i = 'a'; i <= 'z'; i++) {
+			if (sentence.indexOf(i) == -1) {
 				return false;
 			}
 		}

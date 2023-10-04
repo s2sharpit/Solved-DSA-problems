1531f36f1702d8b34f2e552498b87bc7b3a8ef43
Tushar Saini <s2sharpit@gmail.com>
2023-10-04 21:41:55 +0530

    Time: 3 ms (87.69%), Space: 41.7 MB (94.74%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,36 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if(nums2.length < nums1.length)
-		    return intersect(nums2, nums1);
-
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-
-        List<Integer> result = new ArrayList<>();
-        int leftIndex = 0;
-        for(int num: nums1){
-            int index = binarySearch(nums2, num, leftIndex);
-            if(index != -1){
-                result.add(num);
-                leftIndex = index + 1;
-            }
+        int i = 0, j = 0;
+        List<Integer> v = new ArrayList<>();
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                v.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    int binarySearch(int[] nums, int target, int l) {
-        int h = nums.length - 1, i = -1;
-        while (l <= h) {
-            int m = l + (h - l) / 2;
-            if (nums[m] == target) {
-                i = m;
-                h = m - 1;
-            } else if (nums[m] > target) {
-                h = m - 1;
-            } else {
-                l = m + 1;
-            }
-        }
-        return i;
+        int[] ans = new int[v.size()];
+        int k = 0;
+        for (int val : v)
+            ans[k++] = val;
+        return ans;
     }
 }

62b57de74ea071d9b6a75dae10d5942d20ae1829
Tushar Saini <s2sharpit@gmail.com>
2023-10-19 23:42:14 +0530

    Time: 0 ms (100%), Space: 39 MB (61.72%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.java
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -1,16 +1,19 @@
 class Solution {
     public int reachNumber(int target) {
         target = Math.abs(target);
-        int step = 0;
-        int sum = 0;
-        while (sum < target) {
-            step++;
-            sum += step;
+        int l = 0, r = target, numMoves = 0;
+        long finalPosition = 0;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            long sum = (long) m * (m + 1) / 2;
+            if (sum >= target) {
+                finalPosition = sum;
+                numMoves = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
         }
-        while ((sum - target) % 2 != 0) {
-            step++;
-            sum += step;
-        }
-        return step;
+        return ((int)(finalPosition - target)) % 2 == 0 ? numMoves : numMoves % 2 == 0 ? numMoves + 1 : numMoves + 2;
     }
 }
